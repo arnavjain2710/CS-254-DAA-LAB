d691ba28fc65fb3e74544deb86f87bebfebc29f6
arnavjain2710 <119425235+arnavjain2710@users.noreply.github.com>
2024-01-29 15:44:14 +0530

    Question 2 corrected

--- LAB 2/Q-2/main.cpp
+++ LAB 2/Q-2/main.cpp
@@ -1,54 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &A, int m, int n)
-{
-    int i = m - 1;
-    int j = n - 1;
-    int k = m + n - 1;
+void merge(vector<int>& a, int l, int m, int r) {
+    int i = l;
+    int j = m + 1;
 
-    while (i >= 0 && j >= 0)
-    {
-        if (A[i] > A[j])
-            A[k--] = A[i--];
-        else
-            A[k--] = A[j--];
+    while (i <= m && j <= r) {
+        if (a[i] <= a[j]) i++;
+        else {
+            int temp = a[j];
+            for (int k = j; k > i; k--) a[k] = a[k - 1];
+            a[i] = temp;
+            i++, j++, m++;
+        }
     }
-
-    while (j >= 0)
-        A[k--] = A[j--];
 }
-
-int main()
-{
-    int x;
-    vector<int> a, b;
+int main() {
+    int x; vector<int> a, b;
 
     ifstream array_a("../array_a.txt");
-    while (array_a >> x)
-        a.push_back(x);
+    while(array_a >> x) a.push_back(x);
     sort(a.begin(), a.end());
     array_a.close();
-
+   
     ifstream array_b("../array_b.txt");
-    while (array_b >> x)
-        b.push_back(x);
+    while(array_b >> x) b.push_back(x);
     sort(b.begin(), b.end());
     array_b.close();
 
     vector<int> A;
-    for (int y : a)
-        A.push_back(y);
-    for (int y : b)
-        A.push_back(y);
+    for(int y : a) A.push_back(y);
+	for(int y : b) A.push_back(y);
 
-    int n = A.size();
+	int n = A.size();
     int m = a.size();
-    merge(A, m, n - m);
+	merge(A, 0, m-1, n-1);
 
     ofstream outputFile("output.txt");
-    for (int y : A)
-        outputFile << y << " ";
+	for(int y : A) outputFile << y << " ";
     outputFile.close();
-    return 0;
-}
+	return 0;
+} 
